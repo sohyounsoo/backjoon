29e615915392e6fa2b40be1050288b47e3da1bcf
So hyounsoo <sohyounsoo@gmail.com>
2025-03-02 09:01:52 +0900

    스택 알고리즘

---
+++ src/stack/스택_수열_1874.java
@@ -0,0 +1,53 @@
+package stack;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * 스택
+ */
+
+public class 스택_수열_1874 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Stack<Integer> st = new Stack<>();
+        int num = 1;
+        StringBuffer buffer = new StringBuffer();
+
+        boolean flag = false;
+        for (int i = 0; i < n; i++) {
+
+            if(arr[i] >= num) {
+                while(arr[i] >= num) {
+                    st.push(num++);
+                    buffer.append("+\n");
+                }
+                st.pop();
+                buffer.append("-\n");
+            } else {
+                int s = st.pop();
+                if(s > arr[i]) {
+                    System.out.println("NO");
+                    flag = true;
+                    break;
+                } else {
+                    buffer.append("-\n");
+                }
+            }
+        }
+
+        if(!flag) System.out.println(buffer.toString());
+
+
+    }
+
+}
